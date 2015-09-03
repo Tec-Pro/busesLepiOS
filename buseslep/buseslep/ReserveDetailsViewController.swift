dcaa28222d1a515b7ab9edc541b123cf356c28b5
Agustin <agustin@Agustins-Mac-Pro.local>
2015-09-03 12:26:38 -0300

    Reserve Details view

---
+++ buseslep/buseslep/ReserveDetailsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ReserveDetailsViewController.swift
+//  buseslep
+//
+//  Created by Agustin on 9/3/15.
+//  Copyright (c) 2015 Tec-Pro Software. All rights reserved.
+//
+
+import Foundation
