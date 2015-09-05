aab420bddf89748fef21943afc919605f44bdf98
Agustin <agustin@Agustins-Mac-Pro.local>
2015-09-05 18:32:12 -0300

    Reservassss

--- buseslep/buseslep/ReserveDetailsViewController.swift
+++ buseslep/buseslep/ReserveDetailsViewController.swift
@@ -50,6 +50,7 @@ class ReserveDetailsViewController: UIViewController{
     var IdLocalidadDesdeIda: Int = 0
     var IdlocalidadHastaIda: Int = 0
     var CantidadIda: Int = 0
+    var CantidadVuelta: Int = 0
     var IDEmpresaVuelta: Int = 0
     var IDDestinoVuelta: Int = 0
     var CodHorarioVuelta: Int = 0
@@ -63,7 +64,7 @@ class ReserveDetailsViewController: UIViewController{
         super.viewDidLoad()
         let preferences = NSUserDefaults.standardUserDefaults()
         //dni = preferences.objectForKey("dni") as! String
-        dni = "37128116"
+        //dni = "37128116"
         loadIcon.hidden = true
         btnReserve.layer.borderColor = UIColor.blackColor().CGColor
         btnReserve.layer.borderWidth = 0.5
@@ -86,24 +87,29 @@ class ReserveDetailsViewController: UIViewController{
         }
         lblCiudadOrigen.text = self.ciudadOrigen!.nombre!
         lblCiudadDestino.text = self.ciudadDestino!.hasta!
+        lblCiudadOrigenVuelta.text = self.ciudadDestino!.hasta!
+        lblCiudadDestinoVuelta.text = self.ciudadOrigen!.nombre!
         lblHora.text = self.horarioIda!.hora_sale!
         lblFecha.text = self.horarioIda!.fecha_sale!
         lblCantPasajes.text = String(self.CantidadIda)
+        lblCantPasajesVuelta.text = String(self.CantidadIda)
         IDEmpresaIda = horarioIda!.Id_Empresa!
         IdLocalidadDesdeIda = self.ciudadDestino!.id_localidad_origen!
         IdlocalidadHastaIda = self.ciudadDestino!.id_localidad_destino!
         IdLocalidadDesdeVuelta = self.ciudadDestino!.id_localidad_destino!
         IdlocalidadHastaVuelta = self.ciudadDestino!.id_localidad_origen!
         IDDestinoIda = self.horarioIda!.id_destino!
-        
+        CantidadVuelta = CantidadIda
         CodHorarioIda = horarioIda!.cod_horario!
         if horarioVuelta != nil{
             IDEmpresaVuelta = horarioVuelta!.Id_Empresa!
             CodHorarioVuelta = horarioVuelta!.cod_horario!
             IDDestinoVuelta = self.horarioVuelta!.id_destino!
+            lblHoraVuelta.text = self.horarioVuelta!.hora_sale!
+            lblFechaVuelta.text = self.horarioVuelta!.fecha_sale!
         }
         if(!EsIdaVuelta){
-            println("no es ida vuelta")
+            CantidadVuelta = 0
             IDEmpresaVuelta = 0
             IDDestinoVuelta = 0
             CodHorarioVuelta = 0
@@ -125,7 +131,7 @@ class ReserveDetailsViewController: UIViewController{
         var passWS: String = "Lep1234"
         var id_plataforma: Int = 2
    
-        var soapMessage = "<v:Envelope xmlns:i='http://www.w3.org/2001/XMLSchema-instance' xmlns:d='http://www.w3.org/2001/XMLSchema' xmlns:c='http://www.w3.org/2003/05/soap-encoding' xmlns:v='http://schemas.xmlsoap.org/soap/envelope/'><v:Header /><v:Body><n0:AgregarReserva id='o0' c:root='1' xmlns:n0='urn:LepWebServiceIntf-ILepWebService'><userWS i:type='d:string'>\(userWS)</userWS><passWS i:type='d:string'>\(passWS)</passWS><DNI i:type='d:string'>\(dni)</DNI><id_plataforma i:type='d:int'>\(id_plataforma)</id_plataforma><IDEmpresaIda i:type='d:int'>\(IDEmpresaIda)</IDEmpresaIda><IDDestinoIda i:type='d:int'>\(IDDestinoIda)</IDDestinoIda><CodHorarioIda i:type='d:int'>\(CodHorarioIda)</CodHorarioIda><IdLocalidadDesdeIda i:type='d:int'>\(IdLocalidadDesdeIda)</IdLocalidadDesdeIda><IdlocalidadHastaIda i:type='d:int'>\(IdlocalidadHastaIda)</IdlocalidadHastaIda><CantidadIda i:type='d:int'>\(CantidadIda)</CantidadIda><IDEmpresaVuelta i:type='d:int'>\(IDEmpresaVuelta)</IDEmpresaVuelta><IDDestinoVuelta i:type='d:int'>\(IDDestinoVuelta)</IDDestinoVuelta><CodHorarioVuelta i:type='d:int'>\(CodHorarioVuelta)</CodHorarioVuelta><IdLocalidadDesdeVuelta i:type='d:int'>\(IdLocalidadDesdeVuelta)</IdLocalidadDesdeVuelta><IdlocalidadHastaVuelta i:type='d:int'>\(IdlocalidadHastaVuelta)</IdlocalidadHastaVuelta><CantidadVuelta i:type='d:int'>\(CantidadIda)</CantidadVuelta><EsCompra i:type='d:int'>\(EsCompra)</EsCompra></n0:AgregarReserva></v:Body></v:Envelope>" //request para el ws, esto es recomendable copiarlo de Android Studio, sino saber que meter bien, los parametros los paso con \(nombre_vairable)
+        var soapMessage = "<v:Envelope xmlns:i='http://www.w3.org/2001/XMLSchema-instance' xmlns:d='http://www.w3.org/2001/XMLSchema' xmlns:c='http://www.w3.org/2003/05/soap-encoding' xmlns:v='http://schemas.xmlsoap.org/soap/envelope/'><v:Header /><v:Body><n0:AgregarReserva id='o0' c:root='1' xmlns:n0='urn:LepWebServiceIntf-ILepWebService'><userWS i:type='d:string'>\(userWS)</userWS><passWS i:type='d:string'>\(passWS)</passWS><DNI i:type='d:string'>\(dni)</DNI><id_plataforma i:type='d:int'>\(id_plataforma)</id_plataforma><IDEmpresaIda i:type='d:int'>\(IDEmpresaIda)</IDEmpresaIda><IDDestinoIda i:type='d:int'>\(IDDestinoIda)</IDDestinoIda><CodHorarioIda i:type='d:int'>\(CodHorarioIda)</CodHorarioIda><IdLocalidadDesdeIda i:type='d:int'>\(IdLocalidadDesdeIda)</IdLocalidadDesdeIda><IdlocalidadHastaIda i:type='d:int'>\(IdlocalidadHastaIda)</IdlocalidadHastaIda><CantidadIda i:type='d:int'>\(CantidadIda)</CantidadIda><IDEmpresaVuelta i:type='d:int'>\(IDEmpresaVuelta)</IDEmpresaVuelta><IDDestinoVuelta i:type='d:int'>\(IDDestinoVuelta)</IDDestinoVuelta><CodHorarioVuelta i:type='d:int'>\(CodHorarioVuelta)</CodHorarioVuelta><IdLocalidadDesdeVuelta i:type='d:int'>\(IdLocalidadDesdeVuelta)</IdLocalidadDesdeVuelta><IdlocalidadHastaVuelta i:type='d:int'>\(IdlocalidadHastaVuelta)</IdlocalidadHastaVuelta><CantidadVuelta i:type='d:int'>\(0)</CantidadVuelta><EsCompra i:type='d:int'>\(EsCompra)</EsCompra></n0:AgregarReserva></v:Body></v:Envelope>" //request para el ws, esto es recomendable copiarlo de Android Studio, sino saber que meter bien, los parametros los paso con \(nombre_vairable)
         //holaokkkk
         var is_URL: String = "https://webservices.buseslep.com.ar/WebServices/WebServiceLep.dll/soap/ILepWebService" //url del ws
         var lobj_Request = NSMutableURLRequest(URL: NSURL(string: is_URL)!)
@@ -156,8 +162,12 @@ class ReserveDetailsViewController: UIViewController{
                         println(resultCode)
                         dispatch_async(dispatch_get_main_queue(), { () -> Void in
                             if resultCode != "0"{
-                              let alert = UIAlertView(title: "Atencion!", message: "Error al realizar la reserva", delegate:nil, cancelButtonTitle: "Aceptar")
-                              alert.show()
+                                let alert = UIAlertView(title: "Atencion!", message: "Error al realizar la reserva", delegate:nil, cancelButtonTitle: "Aceptar")
+                                alert.show()
+                            }
+                            else{
+                                let alert2 = UIAlertView(title: "Tu Reserva Ha Sido Exitosa!", message: "Te enviamos un mail con los detalles", delegate:nil, cancelButtonTitle: "Aceptar")
+                                alert2.show()
                             }
                             self.loadIcon.hidden = true
                         })
